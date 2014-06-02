e6e77d4feea349c78b72cec21da1044ec9c72f53
ScottyGams <ScottyGammon@gmail.com>
2014-06-02 18:39:43 +0100

    initial commit. My Wingman is live! I am allowed to go forward with my project after approval.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyWingman::Application.config.secret_token = 'bb944379226f00b9b0c1453620c10fe287f2330afe8d5f04d28d435291ea430b27c97e9957e4352f9a333667628ab2e4a94d146de899a52c5a84ba547e9db21b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MyWingman::Application.config.session_store :cookie_store, key: '_MyWingman_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MyWingman::Application.config.session_store :active_record_store

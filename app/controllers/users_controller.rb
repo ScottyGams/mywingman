8654667d6f86fef01607ce445e3ce3603233a704
ScottyGams <ScottyGammon@gmail.com>
2014-06-06 15:05:51 +0100

    added stuff in controller. deleted usesless css file and made a view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      if params[:stripeToken] != nil
+        customer = Stripe::Customer.create(
+          :email => @user.email,
+          :card => params[:stripeToken],
+          :plan => "paid"
+          )
+      end
+      redirect_to root_path, :notice => "Signed up!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update_attributes(params[:user])
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
 end

a47040877c63f2795e134ffbe526868f251be964
ScottyGams <ScottyGammon@gmail.com>
2014-06-09 13:58:22 +0100

    working with omniauth for facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,24 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :blurb, :image, :likes, :location, :name, :phone, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :blurb, :image, :likes, :location, :name, :phone, :email, :password, :password_confirmation, :remember_me, :uid, :provider
+
+  def self.from_omniauth(auth)
+      if user = User.find_by_email(auth.info.email)
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user
+      else
+        where(auth.slice(:provider, :uid)).first_or_create do |user|
+          user.provider = auth.provider
+          user.uid = auth.uid
+          user.email = auth.info.email
+          user.profile_pic = auth.info.image
+          user.password = Devise.friendly_token[0,20]
+      end
+    end
+  end
 end

1c416567686dafcff4b715a6b69520962d193aa6
ScottyGams <ScottyGammon@gmail.com>
2014-06-10 15:32:36 +0100

    now making a model for interests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140609132759) do
+ActiveRecord::Schema.define(:version => 20140610143044) do
+
+  create_table "interests", :force => true do |t|
+    t.text     "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"

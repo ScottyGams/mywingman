dbd577dacb7621e83522a49f357c720829ad7aaa
ScottyGams <ScottyGammon@gmail.com>
2014-06-11 13:41:58 +0100

    making a fake interst maker, not fully functional

--- test/fabricators/interest_fabricator.rb
+++ test/fabricators/interest_fabricator.rb
@@ -1,8 +1,3 @@
 Fabricator(:user) do
-  name { Faker::Name.first_name }
-  password { "testtest" }
-  password_confirmation { |attrs| attrs[:password] }
-  location { Faker::Address.city }
-  phone { (rand(900) + 100).to_s + "-" + (rand(9000) + 1000).to_s + "-" + (rand(9000) + 1000).to_s }
-  email { Faker::Internet.email }
+  name { Faker::SOMETHING!!! }
 end
